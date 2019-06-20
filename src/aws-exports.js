f46080e12297e9a5233eebd1a052a46dc6ab66d7
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-06-19 18:29:31 -0700

    env vars

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,11 +7,11 @@ const awsmobile = {
     "https://najx2aa76zb55jeexyyc43nuyy.appsync-api.us-east-2.amazonaws.com/graphql",
   aws_appsync_region: "us-east-2",
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: APPSYNC_API_KEY || process.env.APPSYNC_API_KEY,
-  aws_cognito_identity_pool_id: IDENTITY_POOL_ID || process.env.IDENTITY_POOL_ID,
+  aws_appsync_apiKey: process.env.APPSYNC_API_KEY || APPSYNC_API_KEY,
+  aws_cognito_identity_pool_id: process.env.IDENTITY_POOL_ID || IDENTITY_POOL_ID,
   aws_cognito_region: "us-east-2",
-  aws_user_pools_id: USER_POOL_ID || process.env.USER_POOL_ID,
-  aws_user_pools_web_client_id: WEB_CLIENT_ID || process.env.WEB_CLIENT_ID,
+  aws_user_pools_id: process.env.USER_POOL_ID || USER_POOL_ID,
+  aws_user_pools_web_client_id: process.env.WEB_CLIENT_ID || WEB_CLIENT_ID,
   oauth: {}
 }
 
